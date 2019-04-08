c7fb9675d15a4e73e561d0cf454e8862c7a5d55b
mariusjagminas <mariusjagminas.it@gmail.com>
2019-04-08 16:48:44 +0200

    Updates

---
+++ precache-manifest.68ef94d62f9a549ce29eb519bd8b519a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c33b881ba10bae8e404",
+    "url": "/mariusjagminas/todo-list/static/js/runtime~main.6c33b881.js"
+  },
+  {
+    "revision": "0f2096f4441417dd4e9f",
+    "url": "/mariusjagminas/todo-list/static/js/main.0f2096f4.chunk.js"
+  },
+  {
+    "revision": "39a0acda35a094cf9c2c",
+    "url": "/mariusjagminas/todo-list/static/js/1.39a0acda.chunk.js"
+  },
+  {
+    "revision": "0f2096f4441417dd4e9f",
+    "url": "/mariusjagminas/todo-list/static/css/main.b9de6850.chunk.css"
+  },
+  {
+    "revision": "9f455cafe6e2e5b7da384d6c4f680cab",
+    "url": "/mariusjagminas/todo-list/index.html"
+  }
+];
